afa586efb700b244e0aeed4a00e2a264b486354c
sk222sw <sk222sw@student.lnu.se>
2014-11-10 15:37:11 +0000

    Some array/object testing that might be kept.

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -6,41 +6,41 @@ window.onload = function(){
 	var birthday = function(date){
 		
 		var userBirthdate = new Date(date);
-		
-		var userBirthday = userBirthdate.getDay();
-		var userBirthmonth = userBirthdate.getMonth() + 1;
+
 		var userBirthyear = userBirthdate.getFullYear();
 		
 		var currentDate = new Date();
-		var currentMonth = currentDate.getMonth() + 1;
-		var currentDay = currentDate.getDate();
 		var currentYear = currentDate.getFullYear();
 		
-		if (userBirthyear < currentYear) {
-			userBirthdate.setFullYear(currentDate.getFullYear());
-		}
-		
-		if (userBirthyear > currentYear) {
-			userBirthdate.setFullYear(currentDate.getFullYear()) +1;
-		}
-		
-		
-		var msDifference =  userBirthdate.getTime() - currentDate.getTime() ;
-		var dayDifference = Math.floor(msDifference / 1000 / 3600 / 24) +1;
-		
-		//Om användaren redan fyllt år så blir det inte -(antal dagar kvar)
-		if (dayDifference < 0) {					
-			dayDifference = dayDifference * 2 +365;
-		}
+		//Kasta undantag om årtalet är NaN eller längre än 4 siffror.
+		if (userBirthdate % 1 !== 0 || userBirthyear.length > 4) {
+			throw new Error("Fel inmatat värde.");
+		}	
+		else {
+			//Om födelseåret är före det nuvaranda så sätts födelseåret till samma som nuvarande år så att uträkningen går rätt till...
+			if (userBirthyear < currentYear) {
+				userBirthdate.setFullYear(currentDate.getFullYear());
+			}
+			
+			//... och samma sak om någon reser i tiden och är född i framtiden.
+			if (userBirthyear > currentYear) {
+				userBirthdate.setFullYear(currentDate.getFullYear()) +1;
+			}
+			
+			//Räknar ut födelsedatumet i ms, och drar bort dagens datum i ms.
+			var msDifference =  userBirthdate.getTime() - currentDate.getTime();
+			//gör om ms till dagar och rundar till heltal. (gjorde ny variabel för
+			//läsbarhetens skull. dumt?)
+			var dayDifference = Math.floor(msDifference / 1000 / 3600 / 24) +1;
+			
+			//Om användaren redan fyllt år så blir det inte -(antal dagar kvar)
+			if (dayDifference < 0) {					
+				dayDifference = dayDifference + 365;
+			}
 		
 		return dayDifference;
-		
-	/*	date = new Date(date);
-		var today = new Date();
-		// var msToDays = 1000 / 3600 / 24;				funkar inte?
-		var birthday = Math.floor(Date.parse(date) / 1000 / 3600 / 24);
-		
-		return Math.floor(birthday - (Date.parse(today) / 1000 / 3600 / 24) + 1); */
+					
+		}
 	};
 	// ------------------------------------------------------------------------------
 
--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,30 @@
 "use strict";
 
+/*
+Din uppgift är att skapa en funktion utan sidoeffekter ("pure function") som tar en array innehållande ett 
+godtyckligt antal objekt (personer med namn och ålder) som argument och returnerar ett nytt objekt. Det objekt 
+som returneras ska innehålla information om den högsta åldern, lägsta åldern, medelåldern samt en sträng med 
+samtliga namn sorterade i bokstavsordning.
+*/
+
 var makePerson = function(persArr){
 
-
-	// Din kod här...
-
+    var persons = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+    
+    persons.names = persons.name;
+    persons.minAge = persons.age;
+    persons.maxAge = persons.age;
+    persons.averageAge = persons.age;
+    
+	var persObj = {};
+	persObj.names;
+	persObj.age;
+	
+	var returnObj = {};
+	returnObj.names;
+	returnObj.minAge;
+	
+    return persons;
+    
 }
 
