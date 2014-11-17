150910a2745233664dbe43a96ee92e71b5a7576a
sk222sw <sk222sw@student.lnu.se>
2014-11-17 13:42:59 +0000

    Moment 2 message class methods etc

--- 2-labbymezzage/script/message.js
+++ 2-labbymezzage/script/message.js
@@ -1,3 +1,33 @@
 "use strict";
 
-function Message () {}
+function Message (message, date) {
+    
+    this.getText = function() {
+        return message;
+    },
+    
+    this.setText = function(_text) {
+        message = _text;
+    };
+    
+    this.getDate = function() {
+        return date;
+    };
+    
+    this.setDate = function(_date) {
+        date = _date;
+    };
+    
+}
+
+Message.prototype.toString = function() {
+    return this.getText()+" ("+this.getDate()+")";
+};
+
+Message.prototype.getHTMLText = function() {
+    return Message.getText;
+};
+
+Message.prototype.getDateText = function() {
+    return Message.getDate;
+};
