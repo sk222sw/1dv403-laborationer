2cd6a660fcf09d9f948c895a496de669a7b3f8df
sk222sw <sk222sw@student.lnu.se>
2014-12-05 15:07:46 +0000

    Added a createMessage function that pushes a new object to the Messages array

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -1,14 +1,28 @@
 "use strict";
 
     var MessageBoard = {
-        
+                
         messages: [],
         
         init:function(e){
+            var submit = document.getElementById("submit");
+            var textarea = document.getElementById("textarea");
             
-            MessageBoard.messages.push("1", 2, 54, "hej");
-            console.log(MessageBoard.messages);
-        }
+            e.preventDefault();
+            
+            submit.onclick = MessageBoard.createMessage;
+
+        },
+        
+        createMessage: function (){
+            var textarea = document.getElementById("textarea");
+            
+            MessageBoard.messages.push(new Message(textarea.value, new Date()));
+            return false; // för att förhindra att sidan defaultladdas om
+        },
     };
+
+            
+
 
 window.onload = MessageBoard.init;
