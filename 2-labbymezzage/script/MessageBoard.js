59eea924c9cfe35faac61e8916ff199fba77aa22
sk222sw <sk222sw@student.lnu.se>
2014-12-05 15:41:59 +0000

    The message is now written out to ONE pre-made div.

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -8,8 +8,6 @@
             var submit = document.getElementById("submit");
             var textarea = document.getElementById("textarea");
             
-            e.preventDefault();
-            
             submit.onclick = MessageBoard.createMessage;
 
         },
@@ -18,8 +16,16 @@
             var textarea = document.getElementById("textarea");
             
             MessageBoard.messages.push(new Message(textarea.value, new Date()));
+            textarea.placeholder = "Skriv ditt meddelande.";
+            var toText = textarea.value;
+            MessageBoard.writeMessage(toText);
             return false; // för att förhindra att sidan defaultladdas om
         },
+        
+        writeMessage: function (message) {
+            var div = document.getElementById("msgText");
+            document.getElementById("msgText").innerHTML = message;
+        }
     };
 
             
