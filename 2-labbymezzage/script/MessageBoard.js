2f33dcb6de95075190b41ecb36eb24f8507f0e5d
sk222sw <sk222sw@student.lnu.se>
2014-12-11 14:18:28 +0000

    Printing out messages now works

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -1,34 +1,51 @@
 "use strict";
 
     var MessageBoard = {
-                
         messages: [],
+        submit: document.getElementById("submit"),
+        textarea: document.getElementById("textarea"),
+        messageDiv: document.getElementById("msgSection"),
         
         init:function(e){
-            var submit = document.getElementById("submit");
-            var textarea = document.getElementById("textarea");
-            
-            submit.onclick = MessageBoard.createMessage;
+            document.getElementById("submit").onclick = MessageBoard.createMessage;
 
         },
         
         createMessage: function (e){
-            MessageBoard.messages.push(new Message(textarea.value, new Date()));
+            MessageBoard.messages.push(new Message(MessageBoard.textarea.value, new Date()));
             MessageBoard.writeMessages(MessageBoard.messages);
+            // console.log(MessageBoard.messages.toString());
             return false; // för att förhindra att sidan defaultladdas om
         },
         
-        writeThisMessage: function(){
+        writeThisMessage: function(thisMess){
+            var div = document.createElement("div");
+            var deleteButton = document.createElement("input");
+            var messageDiv = document.getElementById("msgSection");
+            var mess = document.getElementById("mainsection");
 
+            deleteButton.setAttribute("type", "submit");
+            deleteButton.setAttribute("value", "delete");
+            console.log(MessageBoard.messages[thisMess].toString());
+            
+            div.className = "messageDiv";
+            
+            div.innerHTML = "hej";
+            messageDiv.appendChild(div);
+
+            // div.className = "messageDiv2";
+            // div.innerHTML = MessageBoard.messages[thisMess].toString();
+            // MessageBoard.messageDiv.appendChild(div);
         },
         
+
+        
         writeMessages: function (message) {
-            
-            MessageBoard.textarea.placeholder = "Skriv här";
-            MessageBoard.textarea.value = "";   //Tar bort den inskrivna texten
-            
-            MessageBoard.content.innerHTML = "";  //Raderar alla meddelanden innan loopen skriver ut dem igen
-            
+            // MessageBoard.textarea.placeholder = "Skriv här";
+            // MessageBoard.textarea.value = "";   //Tar bort den inskrivna texten från textarea
+
+            // MessageBoard.messageDiv.innerHTML = "";  //Raderar alla meddelanden innan loopen skriver ut dem igen
+
             for (var i = 0; i < MessageBoard.messages.length; i++) {
                 MessageBoard.writeThisMessage(i);
             }
