0ca5b99b76882fa419c9ce82d4a395e88c813fd0
sk222sw <sk222sw@student.lnu.se>
2015-01-01 21:10:24 +0000

    matchingBricks function(not finished)

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,17 +1,33 @@
 "use strict";
 
 var Memory = {
-    picArray: RandomGenerator.getPictureArray(4, 4),
-
+    turned: 0,     //håller reda på om 1 bricka redan vänts upp.
+    lastTurned: null,    //håller reda på vad förra brickan hade för array-index
+    thisTurned: null,    //håller reda på vad denna bricka har för array-index
+    maxTurned: null,    //används för att kolla när man vinner.    
+    
     init: function(){
-        console.log(Memory.picArray);
-        Memory.generateAll();
+        var cols = 4;   
+        var rows = 4;
+        var picArray = RandomGenerator.getPictureArray(cols, rows);
+        Memory.maxTurned = cols * rows;    //håller reda på hur många brickor som ska vara vända när man vinner.
+        
+        
+        
+        Memory.generateAll(picArray);
     },
     
-    generateAll: function() {
-        for (var i = 0; i < Memory.picArray.length; i++) {
-            var arrayIndex = Memory.picArray[i];
-            var thisPic = arrayIndex + ".png";
+    newGame: function(){
+        var cols = 4;
+        var rows = 4;
+        var picArray = RandomGenerator.getPictureArray(cols, rows);
+        var maxTurned = cols * rows;
+    },
+    
+    generateAll: function(picArray) {
+        for (var i = 0; i < picArray.length; i++) {
+            var arrayIndex = picArray[i];
+            var thisPic = arrayIndex;
             Memory.generatePic(thisPic);
         }
         return false;
@@ -19,7 +35,6 @@ var Memory = {
     
     generatePic: function(thisPic) {
         var memory = document.getElementById("memory");
-
         var picture = document.createElement("img");
         
         picture.setAttribute("src", "memory/pics/" + 0+".png");
@@ -27,25 +42,46 @@ var Memory = {
         
         picture.onclick = function() {
             picture.removeAttribute("src");
-            picture.setAttribute("src", "memory/pics/" + thisPic);
+            picture.setAttribute("src", "memory/pics/" + thisPic + ".png");
             
-            //Om en bricka är vänd redan
-            
-            
-            //Ändra tillbaka efter lite tid
+        console.log(Memory.lastTurned);
+        
+        if (Memory.lastTurned === thisPic) {
+            Memory.matchingBricks();
+        }
+        
+        else {
+        //Ändra tillbaka efter lite tid
             setTimeout(function(){
                 picture.removeAttribute("src");
                 picture.setAttribute("src", "memory/pics/" + 0 + ".png"); 
-            }, 2000);
+            }, 2000);             
+        }
+        
+        Memory.lastTurned = thisPic;
+        
+
             
         };
         
         return false;
     },
     
-    changeURL: function(thisPic){
+    //När två brickor matchas körs denna funktion:
+    matchingBricks: function() {
+        Memory.turned += 2;
+        console.log("Nu äre " + Memory.turned);
+        if (Memory.turned === Memory.maxTurned) {
+            alert("Du vann!");
+        }
     }
+
+    
     
 };
 
 window.onload = Memory.init;
+
+
+
+
