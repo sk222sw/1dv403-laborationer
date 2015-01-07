246d2c35a5ea0b9626514fe520e59e5d10d4d8bb
sk222sw <sk222sw@student.lnu.se>
2015-01-07 23:54:22 +0000

    Bugfix: pics shared IDs and that broke the game

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,9 +5,9 @@
 
 var Memory = {
     turned: 0,     
-    oneTurned: 0,       //håller reda på om 1 bricka redan vänts upp.    
     maxTurned: null,    //används för att kolla när man vinner.    
     twoCardArray: [],   
+    cardPosition: [],
     
     init: function(){
         var cols = 4;   
@@ -15,7 +15,6 @@ var Memory = {
         var picArray = RandomGenerator.getPictureArray(cols, rows);
         Memory.maxTurned = cols * rows;    //håller reda på hur många brickor 
                                            //som ska vara vända när man vinner.
-        console.log("init oneturned är " + Memory.oneTurned);                                   
         Memory.generateAll(picArray);
     },
     
@@ -30,69 +29,103 @@ var Memory = {
     generateAll: function(picArray) {
         for (var i = 0; i < picArray.length; i++) {
             var thisPic = picArray[i];
-            Memory.generatePic(thisPic);
+            var picValue = i;
+            Memory.generatePic(thisPic, picValue);
         }
         return false;
     },
-    
+ //------------------------------------------------------------------------------------------------------  
     //Genererar en bild med dess attribut
-    generatePic: function(thisPic) {
+    generatePic: function(thisPic, picValue) {
         var memory = document.getElementById("memory");
+        var aTag = document.createElement("a");
         var picture = document.createElement("img");
 
-        picture.setAttribute("id", "picture" + thisPic);
+        aTag.setAttribute("href", "#");
+        aTag.setAttribute("rel", "thisPic")
+        picture.setAttribute("id", "picture" + picValue);
+        picture.setAttribute("src", "memory/pics/0.png");
+        picture.setAttribute("value", thisPic);
         picture.setAttribute("class", "defaultImage");
-        memory.appendChild(picture);
+        
+        var picSource = picture.src;    
+
+        memory.appendChild(aTag);
+        aTag.appendChild(picture);
         
             picture.onclick = function() {
+                
                 if (Memory.twoCardArray.length < 2) {
                     
-                    picture.removeAttribute("src");
-                    picture.setAttribute("src", "memory/pics/" + thisPic + ".png");     
-                    
-                    //Om ingen bricka är vänd än    
-                    if (Memory.twoCardArray.length === 0) {
-                        Memory.twoCardArray.push(thisPic);
-                    } else if (Memory.twoCardArray.length === 1) {
-                        Memory.twoCardArray.push(thisPic);
-                        if (Memory.twoCardArray[0] === Memory.twoCardArray[1]) {
-                            //Två matchande brickor
-                            alert("hit");
-                            Memory.twoCardArray = [];
-                        } else {
-                            //Vänd tillbaka
-                            setTimeout(function(){
-                                var pic1 = document.getElementById("picture" + Memory.twoCardArray[0]);
-                                var pic2 = document.getElementById("picture" + Memory.twoCardArray[1]);
+                        if (picture.src === picSource) {
+                            
+                            picture.removeAttribute("src");
+                            picture.setAttribute("src", "memory/pics/" + thisPic + ".png");     
+                            
+                            //Om ingen bricka är vänd än    
+                            if (Memory.twoCardArray.length === 0) {
                                 
-                                pic1.setAttribute("src", "memory/pics/0.png");
-                                pic2.setAttribute("src", "memory/pics/0.png");
-                                Memory.twoCardArray = [];                            
-                            }, 800)
+                                Memory.twoCardArray.push(picValue);
+                                Memory.cardPosition.push(thisPic);
+                                
+                                // console.log("picValue är: " + picValue);
+                                // console.log("Id är: " + thisPic);                            
+                                
+                            } else if (Memory.twoCardArray.length === 1) {
+                                
+                                Memory.twoCardArray.push(picValue);
+                                Memory.cardPosition.push(thisPic);
+                                
+                                console.log("twocardarray0 = " + Memory.twoCardArray[0]);
+                                console.log("twocardarray1 = " + Memory.twoCardArray[1]);
+                                
+                                console.log("picValue är: " + picValue);
+                                console.log("Id är: " + thisPic);                            
+                                
+                                if (Memory.cardPosition[0] === Memory.cardPosition[1]) {
+                                    //Två matchande brickor
+                                    Memory.matchingBricks();
+                                    Memory.twoCardArray = [];
+                                    Memory.cardPosition = [];
     
+                                } else if(Memory.twoCardArray.length === 2) {
+                                    //Vänd tillbaka
+                                    Memory.flipBricks();
+                                }
+                            }
+                        }   else if(Memory.twoCardArray.length === 2) {
+                            Memory.flipBricks();
                         }
-                    }
-                } 
+                }  else {
+                        return 0;
+                }
+                    
             }; //onclick stop
 
         return false;
     },
     
-    changeLastPic: function(){
-        
+    flipBricks: function(){
+        setTimeout(function(){
+            var pic1 = document.getElementById("picture" + Memory.twoCardArray[0]);
+            var pic2 = document.getElementById("picture" + Memory.twoCardArray[1]);
+
+            pic1.setAttribute("src", "memory/pics/0.png");
+            pic2.setAttribute("src", "memory/pics/0.png");
+            Memory.twoCardArray = [];      
+            Memory.cardPosition = [];
+        }, 800);
     },
     
     //När två brickor matchas körs denna funktion:
     matchingBricks: function(thisPic) {
-        Memory.turned += 2;
+            Memory.turned += 2;
+
         console.log("Nu äre " + Memory.turned);
         if (Memory.turned === Memory.maxTurned) {
             alert("Du vann!");
         }
     }
-
-    
-    
 };
 
 window.onload = Memory.init;    
