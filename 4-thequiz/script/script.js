159fe4e34d30f00129b1ee6db52a23ba3b9a57c2
sk222sw <sk222sw@student.lnu.se>
2015-01-09 22:20:46 +0000

    Everything is working + styling

--- 4-thequiz/script/script.js
+++ 4-thequiz/script/script.js
@@ -4,6 +4,7 @@ var quiz = {
     questionText: document.getElementById("questionText"),
     thisQuestion: "http://vhost3.lnu.se:20080/question/1",
     questionNumber: 1,
+    tries: 1,
     
     init: function(){
         quiz.thisQuestion = quiz.getQuestion(quiz.thisQuestion);
@@ -24,15 +25,12 @@ var quiz = {
                             var JSONreturn = xhr.responseText;
                             quiz.thisQuestion = JSON.parse(JSONreturn);
                             
-                            console.log(quiz.thisQuestion);
-                            
                             quiz.displayQuestion(quiz.thisQuestion);
                             // quiz.questionText.innerHTML = thisQuestion.question;
                         } else {
                             console.log("Felkod: " + xhr.status);
                         }
                     }
-                    
                 };
             xhr.open("GET", address, true);
             xhr.send(null);  
@@ -50,73 +48,87 @@ var quiz = {
                             xhr.status <= 300 ||
                             xhr.status === 304) {
 
-                            console.log(xhr.responseText);
                             var JSONreturn = JSON.parse(xhr.responseText);
                             
                             if (JSONreturn.hasOwnProperty("nextURL")) {
                                 quiz.thisQuestion = JSONreturn.nextURL;
-                                quiz.rightOrWrong(JSONreturn.message);
+                                quiz.rightOrWrong(JSONreturn.message, true);
                                 quiz.getQuestion(JSONreturn.nextURL);
                             } else {
                                 quiz.youWin();
                             }
                             
+                            quiz.writeTable(quiz.questionNumber, quiz.tries);
                             
                         } else {
-                            quiz.rightOrWrong("Fel svar!");
+                            quiz.tries+=1;
+                            quiz.rightOrWrong("Fel svar!", false);
                             console.log("felkod: " + xhr.status);
                         }
                     }
-                    
                 };
-
             xhr.open("POST", quiz.thisQuestion.nextURL, true);
 			xhr.setRequestHeader("Content-Type", "application/json");
-			userAnswer = {"answer": userInputArea.value}
+			userAnswer = {"answer": userInputArea.value};
 			xhr.send(JSON.stringify(userAnswer)); 
-            
     },
 
     displayQuestion: function(e){
-        console.log(e);
         quiz.questionText.innerHTML = e.question;
     },
     
-    rightOrWrong: function(message){
-        document.getElementById("rightOrWrong").innerHTML = message;
+    writeTable: function(questionNumber, tries) {
+        var table = document.getElementById("table");
+        var tr = document.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        
+        table.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        td1.innerHTML = questionNumber;
+        td2.innerHTML = tries;
+        
+        quiz.questionNumber += 1;
+        quiz.tries = 1;        
+    },
+    
+    rightOrWrong: function(message, boolValue){
+        var input = document.getElementById("userInputArea");
+        var main = document.getElementById("quizSection");
+
+        var text = document.getElementById("rightOrWrong");
+        text.innerHTML = message;
+        input.value = "";
+        
+        if (!boolValue) {
+            input.classList.add("redBackground");
+            main.classList.add("mainShake");
+
+            setTimeout(function() {
+                
+                input.classList.toggle("redBackground");
+                main.classList.toggle("mainShake");
+                
+                    setTimeout(function() {
+                        
+                        input.classList.toggle("redBackground");
+                        main.classList.toggle("mainShake");     
+                        
+                            setTimeout(function() {
+                                
+                                input.classList.toggle("redBackground");
+                                main.classList.toggle("mainShake");     
+                                text.innerHTML = "";
+                            }, 100);
+                    }, 100);
+            }, 100);
+        }
     },
     
     youWin: function(){
         document.getElementById("rightOrWrong").innerHTML = "Congrats, you win!";
     }
-    
-    
-    
-    
 };
-
 window.onload = quiz.init();
-
-// window.onload = function() {
-    
-//     document.getElementById("btnSubmit").addEventListener("click", function(){
-//         var xhr = new XMLHttpRequest();
-        
-//         xhr.onreadystatechange = function() {
-            
-//             if (xhr.readyState === 4) {
-//                 if (xhr.status >= 200 && 
-//                     xhr.status <= 300 ||
-//                     xhr.status === 304) {
-                    
-//                     var pers = JSON.parse(xhr.responseText);
-                    
-//                     console.log(pers.question);
-//                 }
-//             }
-//         };
-//         xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
-//         xhr.send(null);
-//     });
-    
-// };
