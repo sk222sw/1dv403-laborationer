a984472939d6e1a04035029a49585e54f7d7df4e
sk222sw <sk222sw@student.lnu.se>
2015-01-09 20:37:04 +0000

    Got rid of the unnecessary writeElements function

--- 4-thequiz/script/script.js
+++ 4-thequiz/script/script.js
@@ -3,30 +3,15 @@
 var quiz = {
     questionText: document.getElementById("questionText"),
     thisQuestion: "http://vhost3.lnu.se:20080/question/1",
-    serverResponse: [],
     questionNumber: 1,
     
     init: function(){
         quiz.thisQuestion = quiz.getQuestion(quiz.thisQuestion);
 
-        quiz.writeElements();
         var btnSubmit = document.getElementById("btnSubmit");
         btnSubmit.addEventListener("click", quiz.getUserInput);
     },
-    
-    writeElements: function() {
-        //skapa dom-element
-        
-        //gör variabler av dom-ids
-        var questionNumber = document.getElementById("questionNumber");
-            
-        //set attribut
-        
-        //skriv ut dem
-        questionNumber.innerHTML = "Fråga: " + quiz.questionNumber;
-        
-    },
-    
+
     getQuestion: function(address){
         var xhr = new XMLHttpRequest();
                 xhr.onreadystatechange = function() {
@@ -39,10 +24,7 @@ var quiz = {
                             var JSONreturn = xhr.responseText;
                             quiz.thisQuestion = JSON.parse(JSONreturn);
                             
-
-                            
                             console.log(quiz.thisQuestion);
-                            
                             
                             quiz.displayQuestion(quiz.thisQuestion);
                             // quiz.questionText.innerHTML = thisQuestion.question;
@@ -75,7 +57,11 @@ var quiz = {
                                 quiz.thisQuestion = JSONreturn.nextURL;
                                 quiz.rightOrWrong(JSONreturn.message);
                                 quiz.getQuestion(JSONreturn.nextURL);
+                            } else {
+                                quiz.youWin();
                             }
+                            
+                            
                         } else {
                             quiz.rightOrWrong("Fel svar!");
                             console.log("felkod: " + xhr.status);
@@ -98,6 +84,10 @@ var quiz = {
     
     rightOrWrong: function(message){
         document.getElementById("rightOrWrong").innerHTML = message;
+    },
+    
+    youWin: function(){
+        document.getElementById("rightOrWrong").innerHTML = "Congrats, you win!";
     }
     
     
