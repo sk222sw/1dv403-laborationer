d3e05fb1b1c9bcafa8f799b683709d251f5f1fef
sk222sw <sk222sw@student.lnu.se>
2015-01-21 17:48:38 +0000

    Bugfix: only one imageviewer can be open now

--- 5-pwd/script/ImageViewer.js
+++ 5-pwd/script/ImageViewer.js
@@ -5,6 +5,7 @@
 var ImageViewer = function(){};
     ImageViewer.prototype.pictures = [];
     ImageViewer.prototype.thumbnails = [];
+    ImageViewer.prototype.windowCounter = 0;
     
     ImageViewer.prototype.xhrRequest = function(){
         var that = this;
@@ -90,33 +91,7 @@ var ImageViewer = function(){};
             urlCount += 1;
         }
         
-//         function setOpacity(obj, opacity) {
-//           opacity = (opacity == 100)?99.999:opacity;
-          
-//           // IE/Win
-          
-//           // Safari<1.2, Konqueror
-//           obj.style.KHTMLOpacity = opacity/100;
-          
-//           // Older Mozilla and Firefox
-//           obj.style.MozOpacity = opacity/100;
-          
-//           // Safari 1.2, newer Firefox and Mozilla, CSS3
-//           obj.style.opacity = opacity/100;
-//         }
-//         function fadeIn(objId,opacity) {
-//           if (document.getElementById) {
-//             var obj = document.getElementById(objId);
-//             if (opacity <= 100) {
-//               setOpacity(obj, opacity);
-//               opacity += 10;
-//               window.setTimeout("fadeIn('"+objId+"',"+opacity+")", 100);
-//             }
-//           }
-// }        
-        
     };
-    // image:url(
     ImageViewer.prototype.setBackground = function(JSONurl, thumbDiv){
         
         thumbDiv.addEventListener("click", function(){
@@ -127,11 +102,11 @@ var ImageViewer = function(){};
     };
     
     ImageViewer.prototype.openWindow = function(){
-        Window.prototype.openWindow.call(this);
-        ImageViewer.prototype.xhrRequest();
-
-    };
-    
-    ImageViewer.prototype.getPictures = function(){
         
+        if (ImageViewer.prototype.windowCounter < 1) {
+            Window.prototype.openWindow.call(this);
+            ImageViewer.prototype.xhrRequest();
+            ImageViewer.prototype.windowCounter = 1;
+        }
+
     };
--- 5-pwd/script/Window.js
+++ 5-pwd/script/Window.js
@@ -2,48 +2,54 @@
 
 var Window = function() {};
 
-Window.prototype.counter = 1;
+Window.prototype.counter = 0;
 
 Window.prototype.openWindow = function(name){
-    var main = document.getElementById("main");
-    // var menu = document.createElement("div");
-    var content = document.createElement("div");
-    var topBar = document.createElement("div");
-    var secondTop = document.createElement("div");
-    var statusBar = document.createElement("div");
-    var frame = document.createElement("div");
-    var closeButton = document.createElement("div");
-    var responseArray = [];
-    var loading = document.createElement("div");
-    
-    frame.id = "frame";
-    closeButton.classList.add("windowClose");
-    frame.classList.add("newWindow");
-    content.classList.add("windowContent");
-    content.setAttribute("id", "windowContent");
-    topBar.classList.add("windowTopBar");
-    secondTop.classList.add("windowSecondTopBar");
-    statusBar.classList.add("windowStatusBar");
-    loading.setAttribute("id", "loading");
-    
-    secondTop.innerHTML = "Image Viewer";
-    
-    closeButton.addEventListener("click", Window.prototype.closeWindow, false);
-    
-    frame.appendChild(topBar);
-    frame.appendChild(secondTop);
-    frame.appendChild(content);
-    statusBar.appendChild(loading);
-    frame.appendChild(statusBar);
-    secondTop.appendChild(closeButton);
-    main.appendChild(frame);
-    Window.prototype.counter += 1;
+
+        var main = document.getElementById("main");
+        // var menu = document.createElement("div");
+        var content = document.createElement("div");
+        var topBar = document.createElement("div");
+        var secondTop = document.createElement("div");
+        var statusBar = document.createElement("div");
+        var frame = document.createElement("div");
+        var closeButton = document.createElement("div");
+        var responseArray = [];
+        var loading = document.createElement("div");
+        
+        frame.id = "frame";
+        closeButton.classList.add("windowClose");
+        frame.classList.add("newWindow");
+        content.classList.add("windowContent");
+        content.setAttribute("id", "windowContent");
+        topBar.classList.add("windowTopBar");
+        secondTop.classList.add("windowSecondTopBar");
+        statusBar.classList.add("windowStatusBar");
+        loading.setAttribute("id", "loading");
+        
+        secondTop.innerHTML = "Image Viewer";
+        
+        closeButton.addEventListener("click", Window.prototype.closeWindow, false);
+        
+        frame.appendChild(topBar);
+        frame.appendChild(secondTop);
+        frame.appendChild(content);
+        statusBar.appendChild(loading);
+        frame.appendChild(statusBar);
+        secondTop.appendChild(closeButton);
+        main.appendChild(frame);
+        Window.prototype.counter += 1;
+
 };
 
 Window.prototype.closeWindow = function(){
     var main = document.getElementById("main");
     var frame = document.getElementById("frame");
     main.removeChild(frame);
+
+    //Sätt tillbaka windowCounter till 0 så man kan öppna ett nytt fönster när det första är stängt
+    ImageViewer.prototype.windowCounter = 0;
+
     console.log("closed");  
     return false;
 };
